d66b0ebcc7c86c68575a9e80c2690e43e79dbe58
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-01 15:09:58 +0100

    Added networked player control to scene

---
+++ BR Unity PC/Assets/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerInput : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var z = Input.GetAxis ("Vertical") * 0.1f;
+		var x = Input.GetAxis ("Horizontal") * 0.1f;
+		transform.Translate (x, 0, z);
+	}
+}
