3a83a81a44ce11dc193a819ec7581ce8943edd87
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-01 20:16:32 +0100

    Added SamDraw scene to prevent merge conflicts and fixed some of previously lost code

--- BR Unity PC/Assets/PlayerInput.cs
+++ BR Unity PC/Assets/PlayerInput.cs
@@ -4,15 +4,10 @@ using UnityEngine.Networking;
 
 public class PlayerInput : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		if (!isLocalPlayer)
-			return;
+		Debug.Log ("Update");
+		Debug.Log ("Going to move");
 		var x = Input.GetAxis ("Horizontal");
 		var z = Input.GetAxis ("Vertical");
 		transform.Translate (x, 0, z);
