80fbe87ee35da9f850dd11a3732f6dc9ae2c6afd
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-01 23:26:56 +0100

    Added FOV and trigger area to monster

--- BR Unity PC/Assets/Hunter.cs
+++ BR Unity PC/Assets/Hunter.cs
@@ -5,12 +5,36 @@ using UnityEngine.Networking;
 public class Hunter : NetworkBehaviour {
 
 	NavMeshAgent agent;
+	float time = 0f;
+	public float fov = 60.0f;
+
+	bool SetDestination(Transform destination) {
+		time = 0f;
+		if (LineOfSight (destination)) {
+			agent.SetDestination (destination.position);
+			return agent.hasPath;
+		} else
+			return false;
+	}
+
+	bool LineOfSight (Transform target) {
+		RaycastHit hit = new RaycastHit();
+		return Vector3.Angle (target.position - transform.position, transform.forward) <= fov
+			&& Physics.Linecast (transform.position, target.position, out hit)
+			&& hit.collider.transform == target;
+	}
+
+	void OnTriggerStay(Collider collider) {
+		time += Time.deltaTime;
+		if (time >= 0.25f) SetDestination (collider.transform);
+	}
 
 	void OnTriggerEnter(Collider collider) {
-		agent.SetDestination (collider.transform.position);
+		SetDestination (collider.transform);
 	}
 
 	void OnTriggerExit(Collider collider) {
+		time = 0f;
 		agent.Stop ();
 		agent.ResetPath ();
 	}
