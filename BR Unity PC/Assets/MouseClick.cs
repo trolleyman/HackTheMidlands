6963d4d4e19695d5b4f51ba89ca3e0da04c2877d
Jack Morrison <Jack Morrison>
2016-10-01 13:06:03 +0100

    Started adding mouseclick response

---
+++ BR Unity PC/Assets/MouseClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.mousePosition (0)) {
+			Vector3 a = Input.mousePosition;
+		}
+		Debug.Log (a);
+	}
+}
