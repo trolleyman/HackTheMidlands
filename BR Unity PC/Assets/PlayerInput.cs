2e4958c7eae8d4fcdfe9713e7ce3e838ffc0e488
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-01 15:58:54 +0100

    Fixed issue where player movement isn't propogated to client

--- BR Unity PC/Assets/PlayerInput.cs
+++ BR Unity PC/Assets/PlayerInput.cs
@@ -11,6 +11,8 @@ public class PlayerInput : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer)
+			return;
 		var z = Input.GetAxis ("Vertical") * 0.1f;
 		var x = Input.GetAxis ("Horizontal") * 0.1f;
 		transform.Translate (x, 0, z);
