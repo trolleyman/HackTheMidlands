dde07325f7aa0d994871c1edf199a01f60cbeaef
Jack Morrison <Jack Morrison>
2016-10-01 13:23:42 +0100

    Saved project and adjusted clicking

--- BR Unity PC/Assets/MouseClick.cs
+++ BR Unity PC/Assets/MouseClick.cs
@@ -11,9 +11,25 @@ public class MouseClick : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.mousePosition (0)) {
+
+		 
+		if (Input.GetMouseButtonDown(0)) {
 			Vector3 a = Input.mousePosition;
+			Debug.Log (a);
+			Debug.Log ("First point recorded");
+
+			IEnumerator WaitForInstruction;
+			if (Input.GetMouseButtonDown (0)) {
+				Vector3 b = Input.mousePosition;
+				Debug.Log (b);
+				Debug.Log ("Second point recorded");
+			}
+
+			if (Input.GetMouseButtonDown (1)) {
+				Debug.Log ("Line cancelled");
+			}
+
 		}
-		Debug.Log (a);
+
 	}
 }
