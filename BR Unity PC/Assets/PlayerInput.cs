039c5661a7225398318fbb922f865b3f0f7e8ad8
SamTebbs33 <samuel.tebbs@gmail.com>
2016-10-02 00:14:30 +0100

    Made object destruction and creation sync to clients

--- BR Unity PC/Assets/PlayerInput.cs
+++ BR Unity PC/Assets/PlayerInput.cs
@@ -6,6 +6,7 @@ public class PlayerInput : NetworkBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown ("space")) GameObject.Destroy( GameObject.Find ("Cube"));
 		var x = Input.GetAxis ("Horizontal") * 0.1f;
 		var z = Input.GetAxis ("Vertical") * 0.1f;
 		transform.Translate (x, 0, z);
