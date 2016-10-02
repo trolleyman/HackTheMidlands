0555777a9b7a29cd5e20d8c1878ec3795c18d91c
Callum Tolley <cgtrolley@gmail.com>
2016-10-02 06:27:04 +0100

    Some maze trials

---
+++ BR Unity Android/Assets/BR/Assets/MouseClick.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+
+	public GameObject StrongWall;
+	public GameObject WeakWall;
+	public GameObject Wall;
+	public GameObject Goal;
+	public GameObject Empty;
+
+	private bool isDown1;
+	private Vector3 mousePosFirst;
+	private Vector3 mousePosSecond;
+
+	private GameObject[,] board;
+	private const int BOARD_X_OFFSET = 10;
+	private const int BOARD_Z_OFFSET = 5;
+	private const int BOARD_W = 20;
+	private const int BOARD_H = 10;
+
+	private int CURRENT_WWALL = 30;
+	private int CURRENT_SWALL = 20;
+	private int CURRENT_GOAL = 1;
+
+	private const int sWall = 1, wWall = 2, goal = 3;
+	private int currentBlock = sWall;
+
+
+	// Use this for initialization
+	void Start () {
+		board = new GameObject[BOARD_W, BOARD_H];
+
+		for (int x = 0; x < BOARD_W; x++) {
+			for (int z = 0; z < BOARD_H; z++) {
+				board [x, z] = null;
+			}
+		}
+
+		for (int x = 0; x < BOARD_W; x++) {
+			createObject(x, 0, Wall);
+			Debug.Log (x + ", 0");
+		}
+		for (int x = 0; x < BOARD_W; x++) {
+			createObject(x, BOARD_H - 1, Wall);
+			Debug.Log (x + ", 9");
+		}
+		for (int z = 0; z < BOARD_H; z++) {
+			createObject(0, z, Wall);
+		}
+		for (int z = 0; z < BOARD_H; z++) {
+			createObject(BOARD_W - 1, z, Wall);
+		}
+
+	}
+
+	void createObject(int x, int z, GameObject go) {
+		if (board [x, z] == null) {
+
+			if (go == StrongWall && CURRENT_SWALL > 0) {
+				board [x, z] = buildStrongWall (x - BOARD_X_OFFSET, z - BOARD_Z_OFFSET);
+				CURRENT_SWALL--;
+			} else if (go == WeakWall && CURRENT_WWALL > 0) {
+				board [x, z] = buildWeakWall (x - BOARD_X_OFFSET, z - BOARD_Z_OFFSET);
+				CURRENT_WWALL--;
+			} else if (go == Goal && CURRENT_GOAL > 0) {
+				board [x, z] = buildGoal (x - BOARD_X_OFFSET, z - BOARD_Z_OFFSET);
+				CURRENT_GOAL--;
+			} else if (go == Wall) {
+				board [x, z] = buildWall (x - BOARD_X_OFFSET, z - BOARD_Z_OFFSET);
+			} else if (go == Empty) {
+				Debug.Log ("Nothing to remove");
+			}
+		} else {
+			if (go == Empty && (board [x, z].tag != "Invulnerable")) {
+				if (board [x, z].tag == "WeakWall") {
+					CURRENT_WWALL++;
+				} else if (board [x, z].tag == "StrongWall") {
+					CURRENT_SWALL++;
+				} else if (board [x, z].tag == "Goal") {
+					CURRENT_GOAL++;
+				}
+				board [x, z] = removeWall (x, z);
+			} else {
+				Debug.Log ("Not a valid position");
+			}
+		}
+			
+	}
+
+	public GameObject removeWall(int x, int y) {
+		Destroy (board [x, y]);
+		return null;
+	}
+
+	public GameObject buildWall(float x, float z) {
+		Vector3 pos = new Vector3(x, 0.0f, z);
+		GameObject wall = Instantiate (Wall, pos, Quaternion.identity) as GameObject;
+		return wall;
+	}
+
+	public GameObject buildStrongWall(float x, float z) {
+		Vector3 pos = new Vector3(x, 0.0f, z);
+		GameObject strongWall = Instantiate (StrongWall, pos, Quaternion.identity) as GameObject;
+		return strongWall;
+	}
+
+	public GameObject buildWeakWall(float x, float z) {
+		Vector3 pos = new Vector3(x, 0.0f, z);
+		GameObject weakWall = Instantiate (WeakWall, pos, Quaternion.identity) as GameObject;
+		return weakWall;
+	}
+
+	public GameObject buildGoal(float x, float z) {
+		Vector3 pos = new Vector3(x, 0.0f, z);
+		GameObject gl = Instantiate (Goal, pos, Quaternion.identity) as GameObject;
+		return gl;
+	}
+
+	public GameObject intToGO(int current) {
+		if (current == sWall) {
+			return StrongWall;
+		} else if (current == wWall) {
+			return WeakWall;
+		} else {
+			return Goal;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			var mousePosSecond = Input.mousePosition;
+			mousePosSecond.z = 10.0f;
+			mousePosSecond = Camera.main.ScreenToWorldPoint(mousePosSecond);
+			int x = (int)Mathf.Round(mousePosSecond.x);
+			int z = (int)Mathf.Round(mousePosSecond.z);
+			Debug.Log ("Second point recorded" + mousePosSecond);
+
+			createObject (x + BOARD_X_OFFSET, z + BOARD_Z_OFFSET, intToGO(currentBlock));
+		}
+		if (Input.GetMouseButton (1)) {
+			var mousePosSecond = Input.mousePosition;
+			mousePosSecond.z = 10.0f;
+			mousePosSecond = Camera.main.ScreenToWorldPoint(mousePosSecond);
+			int x = (int)Mathf.Round(mousePosSecond.x);
+			int z = (int)Mathf.Round(mousePosSecond.z);
+			Debug.Log ("Second point recorded" + mousePosSecond);
+
+			createObject (x + BOARD_X_OFFSET, z + BOARD_Z_OFFSET, Empty);
+		}
+		if (Input.GetKeyDown ("1")) {
+			currentBlock = sWall;
+			Debug.Log ("Strong wall selected");
+		}
+		if (Input.GetKeyDown ("2")) {
+			currentBlock = wWall;
+			Debug.Log ("Weak wall selected");
+		}
+		if (Input.GetKeyDown ("3")) {
+			currentBlock = goal;
+			Debug.Log ("Goal selected");
+		}
+	}
+}
---
+++ BR Unity Android/Assets/MazeGenerator.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BoardPiece {
+	EMPTY,
+	STRONG_WALL,
+	WEAK_WALL,
+	MONSTER_SPAWNER
+}
+
+public struct IVector2 {
+	public int x;
+	public int y;
+
+	public IVector2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public bool Equals(Object o) {
+		return o is IVector2 && this == (IVector2)o;
+	}
+};
+
+public class MazeGenerator : MonoBehaviour {
+
+	public GameObject mazeParent;
+	public GameObject strongWallPrefab;
+	public GameObject weakWallPrefab;
+
+	private BoardPiece[,] board;
+	private const int BOARD_W = 6;
+	private const int BOARD_H = 6;
+
+	private const int START_X = 0;
+	private const int START_Y = 0;
+
+	private const int GOAL_X = BOARD_W;
+	private const int GOAL_Y = BOARD_H / 2;
+
+	private IVector2 goal;
+
+	// Use this for initialization
+	void Start () {
+		goal = new IVector2 (GOAL_X, GOAL_Y);
+		board = new BoardPiece[BOARD_W, BOARD_H];
+		for (int x = 0; x < BOARD_W; x++)
+			for (int y = 0; y < BOARD_H; y++)
+				board [x, y] = BoardPiece.STRONG_WALL;
+
+		Stack<IVector2> s = new Stack<IVector2> ();
+		s.Push(new IVector2(START_X, START_Y));
+		while (s.Count != 0) {
+			IVector2 pos = s.Pop ();
+
+			Debug.Log ("pos: " + pos.x + "," + pos.y + ", BOARD_W:" + BOARD_W);
+
+			int numEmptyAdjacent = 0;
+			if (pos.x != 0 && board [pos.x - 1, pos.y] == BoardPiece.EMPTY)
+				numEmptyAdjacent++;
+			if (pos.y != 0 && board [pos.x, pos.y - 1] == BoardPiece.EMPTY)
+				numEmptyAdjacent++;
+			if (pos.x != BOARD_W - 1 && board [pos.x + 1, pos.y] == BoardPiece.EMPTY)
+				numEmptyAdjacent++;
+			if (pos.y != BOARD_H - 1 && board [pos.x, pos.y + 1] == BoardPiece.EMPTY)
+				numEmptyAdjacent++;
+			
+			if (numEmptyAdjacent <= 1)
+				GetNeighbours (pos, s);
+		}
+
+		// Display maze
+		for (int x = 0; x < BOARD_W; x++) {
+			for (int y = 0; y < BOARD_H; y++) {
+				switch (board[x, y]) {
+				case BoardPiece.STRONG_WALL: {
+						GameObject strongWall = Instantiate (strongWallPrefab, new Vector3 (x, 1.0f, y), Quaternion.identity) as GameObject;
+						strongWall.transform.parent = mazeParent.transform;
+						break;
+					}
+				case BoardPiece.WEAK_WALL: {
+						GameObject weakWall = Instantiate (weakWallPrefab, new Vector3 (x, 1.0f, y), Quaternion.identity) as GameObject;
+						weakWall.transform.parent = mazeParent.transform;
+						break;
+					}
+				default:
+					break;
+				}
+			}
+		}
+	}
+
+	void GetNeighbours(IVector2 pos, Stack<IVector2> s) {
+		int i = 0;
+		IVector2[] vs = new IVector2[4];
+
+		if (pos.x > 0) {
+			vs[i] = new IVector2(pos.x - 1, pos.y);
+			if (vs [i].Equals(goal))
+				s.Push (vs [i]);
+			else
+				i += 1;
+		}
+		if (pos.y > 0) {
+			vs[i] = new IVector2(pos.x, pos.y - 1);
+			if (vs [i].Equals(goal))
+				s.Push (vs [i]);
+			else
+				i += 1;
+		}
+		if (pos.x < BOARD_W - 1) {
+			vs[i] = new IVector2(pos.x + 1, pos.y);
+			if (vs [i].Equals(goal))
+				s.Push (vs [i]);
+			else
+				i += 1;
+		}
+		if (pos.y < BOARD_H - 1) {
+			vs[i] = new IVector2(pos.x, pos.y + 1);
+			if (vs [i].Equals(goal))
+				s.Push (vs [i]);
+			else
+				i += 1;
+		}
+
+		Shuffle (vs, i);
+		for (int j = 0; j < i; j++) {
+			s.Push (vs [j]);
+		}
+	}
+
+	void Shuffle<T> (T[] array, int n) {
+		while (n > 1) {
+			int k = Mathf.Min(Mathf.RoundToInt(Random.value * (n)), n - 1);
+			Debug.Log ("k: " + k + ", n: " + n);
+			n--;
+			T temp = array [n];
+			array [n] = array [k];
+			array [k] = temp;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
